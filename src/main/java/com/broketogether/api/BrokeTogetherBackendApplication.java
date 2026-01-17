453d25c9a2e637a25a3a3e013cecfed1143e918c
Abdullah Tariq <abdullahtariq096@gmail.com>
2026-01-16 19:18:16 -0800

    Updating parameters

--- src/main/java/com/broketogether/api/BrokeTogetherBackendApplication.java
+++ src/main/java/com/broketogether/api/BrokeTogetherBackendApplication.java
@@ -1,14 +1,17 @@
 package com.broketogether.api;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class BrokeTogetherBackendApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(BrokeTogetherBackendApplication.class, args);
   }
 
